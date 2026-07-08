e6c52dca25f8cdd0ab4e1e8c2af459b703716e67
bongacat07 <vaibhavanandeh07@gmail.com>
2026-07-08 13:38:19 +0530

    All syscalls needed for a server added

--- src/syscall.zig
+++ src/syscall.zig
@@ -49,3 +49,115 @@ pub fn close(fd: types.Fd) !void {
         };
     }
 }
+
+pub fn listen(fd: types.Fd, backlog: i32) !void {
+    if (backlog < 0) return error.InvalidArgument;
+
+    const ret = linux_syscall.syscall2(
+        SYS.listen,
+        @as(usize, @intCast(fd.fd)),
+        @as(usize, @intCast(backlog)),
+    );
+    if (ret < 0) {
+        return switch (errno.fromRet(ret)) {
+            .ADDRINUSE => error.AddressInUse,
+            .BADF => error.InvalidFD,
+            .NOTSOCK => error.NotASocketFD,
+            else => error.Unexpected,
+        };
+    }
+}
+
+pub fn bind(fd: types.Fd, addr: *const types.SockAddr, addrlen: u32) !void {
+    const ret = linux_syscall.syscall3(
+        SYS.bind,
+        @as(usize, @intCast(fd.fd)),
+        @intFromPtr(addr),
+        @as(usize, @intCast(addrlen)),
+    );
+
+    if (ret < 0) {
+        return switch (errno.fromRet(ret)) {
+            .BADF => error.InvalidFD,
+            .NOTSOCK => error.NotASocketFD,
+            .ADDRINUSE => error.AddressInUse,
+            .ADDRNOTAVAIL => error.AddressNotAvailable,
+            .ACCES => error.AccessDenied,
+            .INVAL => error.InvalidArgument,
+            else => error.Unexpected,
+        };
+    }
+}
+
+pub fn accept(fd: types.Fd, addr: ?*types.SockAddr, addrlen: ?*u32) !types.Fd {
+    const ret = linux_syscall.syscall3(
+        SYS.accept,
+        @as(usize, @intCast(fd.fd)),
+        if (addr) |a| @intFromPtr(a) else 0,
+        if (addrlen) |l| @intFromPtr(l) else 0,
+    );
+
+    if (ret < 0) {
+        return switch (errno.fromRet(ret)) {
+            .BADF => error.InvalidFD,
+            .NOTSOCK => error.NotASocketFD,
+            .OPNOTSUPP => error.OperationNotSupported,
+            .AGAIN => error.WouldBlock,
+            .CONNABORTED => error.ConnectionAborted,
+            .MFILE, .NFILE => error.SystemResources,
+            .PERM => error.AccessDenied,
+            else => error.Unexpected,
+        };
+    }
+
+    return .{ .fd = @intCast(ret) };
+}
+
+pub fn read(fd: types.Fd, buf: []u8) !usize {
+    const ret = linux_syscall.syscall3(
+        SYS.read,
+        @as(usize, @intCast(fd.fd)),
+        @intFromPtr(buf.ptr),
+        buf.len,
+    );
+
+    if (ret < 0) {
+        return switch (errno.fromRet(ret)) {
+            .BADF => error.InvalidFD,
+            .FAULT => error.BadBuffer,
+            .INTR => error.Interrupted,
+            .INVAL => error.InvalidArgument,
+            .IO => error.IOError,
+            .AGAIN => error.WouldBlock,
+            .CONNRESET => error.ConnectionReset,
+            else => error.Unexpected,
+        };
+    }
+
+    return @intCast(ret);
+}
+
+pub fn write(fd: types.Fd, buf: []const u8) !usize {
+    const ret = linux_syscall.syscall3(
+        SYS.write,
+        @as(usize, @intCast(fd.fd)),
+        @intFromPtr(buf.ptr),
+        buf.len,
+    );
+
+    if (ret < 0) {
+        return switch (errno.fromRet(ret)) {
+            .BADF => error.InvalidFD,
+            .FAULT => error.BadBuffer,
+            .INTR => error.Interrupted,
+            .INVAL => error.InvalidArgument,
+            .IO => error.IOError,
+            .AGAIN => error.WouldBlock,
+            .PIPE => error.BrokenPipe,
+            .CONNRESET => error.ConnectionReset,
+            else => error.Unexpected,
+        };
+    }
+
+    return @intCast(ret);
+}
--- src/types.zig
+++ src/types.zig
@@ -1,5 +1,3 @@
-const errno = @import("errno.zig");
-
 pub const Fd = struct {
     fd: i32,
 };
@@ -11,12 +9,32 @@ pub const SocketType = enum(c_int) {
 };
 
 pub const AddressFamily = enum(c_int) {
+    unix = 1,
     inet = 2,
     inet6 = 10,
-    unix = 1,
 };
 
 pub const Protocol = enum(c_int) {
     tcp = 6,
     udp = 17,
 };
+
+pub const SockAddrIn = extern struct {
+    family: u16 = @intFromEnum(AddressFamily.inet),
+    port: u16,
+    addr: u32,
+    zero: [8]u8 = [_]u8{0} ** 8,
+};
+
+pub const SockAddr = extern struct {
+    family: u16,
+    sa_data: [14]u8,
+};
+
+pub fn htons(port: u16) u16 {
+    return @byteSwap(port);
+}
+
+pub fn htonl(addr: u32) u32 {
+    return @byteSwap(addr);
+}
